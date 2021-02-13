63e20ef77d428a3df7d6d4ea0ea6fa6f523375f9
Stranger01 <joseantoniosysnet@gmail.com>
2021-02-12 20:42:29 -0500

    updated layout in nav header

--- app/src/main/java/co/tk/funciona/MainActivity.kt
+++ app/src/main/java/co/tk/funciona/MainActivity.kt
@@ -1,21 +1,13 @@
 package co.tk.funciona
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,10 +16,17 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+
+        fun loginBtnNavClicked(view: View) {
+
+        }
+
+        fun addChannelClicked(view: View) {
+
+        }
+
+        fun sendMsgBtnClicked(view: View){
+
         }
     }
 
